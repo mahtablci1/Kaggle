d6f8dc70d0c12e7f6a6100ab28157ac636afd490
Keshav Varadachari <Keshav.Varadachari@thomsonreuters.com>
2020-04-06 18:24:19 -0400

    feat(viz): update viz
    * sort by publish time and
    * show relevant risk factors

--- viz/react-viz/src/App.js
+++ viz/react-viz/src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import PaperComponent from './Paper/Paper.js';
-import data from './summaries.json';
+import data from './risk_covid_join.json'
 
 class App extends Component {
-	state = {
-		paperRefs: data,
-		winSize: 10
+
+	constructor() {
+		super();
+		// sort papers by whethere they're covid related
+		let _papers = data.sort((p1, p2) => {
+			if (p1.is_covid_related && !p2.is_covid_related) return -1;
+			else if (p2.is_covid_related && !p1.is_covid_related) return 1;
+			else if (p1.is_covid_related && p2.is_covid_related) {
+				return p2.publish_time - p1.publish_time
+			}
+			else return 0;
+		})
+
+		// clean risk_factor column
+		_papers.forEach(p => {
+			// use double quotes
+			p.risk_factor = p.risk_factor.replace(/\'/g, '"')
+			// remove duplicates
+			p.risk_factor = [...new Set(JSON.parse(p.risk_factor))]
+		});
+
+		_papers = _papers.reduce((acc, paper) => {
+			if (acc[paper.doc_id]) {
+				acc[paper.doc_id].risk_factor.push(...paper.risk_factor)
+			} else {
+				acc[paper.doc_id] = paper
+			}
+			return acc;
+		}, {})
+
+		const papers = Object.values(_papers)
+		console.log(papers);
+		this.state = {
+			paperRefs: papers,
+			winSize: 10
+		}
 	}
+
 
 	prevArticleHandler = () => {
 		if (this.state.curIndex > 0) {
@@ -44,15 +78,13 @@ class App extends Component {
 		const papers = this.state.paperRefs.slice(this.state.curIndex,
 			this.state.winSize);
 
-
-		console.log(this.state.paperRefs);
+		console.log(papers)
 
 
 		return (
 			<div>
 				<div className="app-header">
-					<h1>Hey, we need your help!</h1>
-					<h4>Please select the sentences in each summary that are relevant to the following Covid-19 risk factors</h4>
+					<h1>What do we know about COVID-19 risk factors? What have we learned from epidemiological studies?</h1>
 				</div>
 
 				<div>

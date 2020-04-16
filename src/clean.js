f8c4df870296239cd25e3c33f487924f8a0f2d54
nikhil.budathoki <git config --global user.email {ID}+{username}@users.noreply.github.com>
2020-04-16 16:41:49 -0400

    Restored data again

--- src/clean.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('./data/enriched_covid_df.json.json', (err, data) => {
-  console.log(err);
-  fs.writeFileSync('./data/enriched_covid_df.json_spaced.json', JSON.stringify(JSON.parse(data), null, 2))
-})
